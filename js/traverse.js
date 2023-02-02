e40261164852b3522452d2654f6422a7e0151a0c
nightzillla <81122802+nightzillla@users.noreply.github.com>
2023-02-01 21:25:27 -0600

    learning array.from forEach

--- js/traverse.js
+++ js/traverse.js
@@ -1,3 +1,22 @@
-const grandparent = document.getElementById("grandparent-id")
+// NOTES YOU NEED TO PUT Array.from() if you want to use forEach Method
+// Getting by the ID
+// const grandparent = document.getElementById("grandparent-id")
+const grandparent = document.querySelector(".grandparent")
+const parents = Array.from(grandparent.children)
+parents.forEach(changeColor);
+// changeColor(grandparent);
 
-grandparent.style.backgroundColor = "#333"
+// const parents = document.querySelectorAll(".parent")
+// parents.forEach(changeColor)
+// changeColor(parents)
+// Getting by the className
+// const parents = document.getElementsByClassName('parent')
+
+// using forEach with className
+// const parents = Array.from(document.getElementsByClassName('parent'))
+// parents.forEach(changeColor)
+// changeColor(grandparent)
+
+function changeColor(element) {
+    element.style.backgroundColor = "#333"
+}
